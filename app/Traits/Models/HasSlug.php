20f108f17f7cc83d5a005c8ce081538f90d41f41
Sasha <sashapozhidaev07@gmail.com>
2023-11-01 17:07:30 +0600

    slug unique and client callback refactor

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -12,19 +12,53 @@ trait HasSlug
     protected static function bootHasSlug(): void
     {
         static::creating(function (Model $item) {
-            $slug = Str::slug($item->{self::slugFrom()});
-            $countSlug = $item->where('slug', $slug)->count();
-
-            if ($countSlug > 0) {
-                $slug .= '-' . ($countSlug + 1);
-            }
-
-            $item->slug = $slug;
+            $item->makeSlug();
         });
     }
+    
+    protected function makeSlug(): void
+    {
+        if ($this->{$this->slugColumn()} == false) {
+            $slug = $this->makeSlugUnique(
+                str($this->{$this->slugFrom()})
+                    ->slug()
+                    ->value()
+            );
 
-    public static function slugFrom(): string
+            $this->{$this->slugColumn()} = $slug;
+        }
+    }
+
+    protected static function slugFrom(): string
     {
         return 'title';
+    }
+    
+    protected static function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    private function makeSlugUnique(string $slug): string
+    {
+        $originalSlug = $slug;
+        $count = 0;
+
+        while ($this->isSlugExists($slug)) {
+            $count++;
+            $slug = $originalSlug . '-' . $count;
+        }
+
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            ->where($this->getKeyName(), '!=', $this->getKey())
+            ->withoutGlobalScopes();
+
+        return $query->exists();
     }
 }
